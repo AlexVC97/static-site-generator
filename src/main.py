d3eb853865ee91ae8b2e3a5abbc45ef659999aeb
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-05-20 11:40:04 +0200

    created new TextNode object

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+# Importing my own files
+from textnode import TextNode, TextType
+
+# Functions
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
+
+# Program starts here
+if __name__ == "__main__":
+    main()
