35ca516528ea179dbc6b6b898f634a600da36d90
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-05-26 10:48:51 +0200

    verify block_to_block_type handles all block types correctly

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,7 +1,7 @@
 import unittest
 
 # Importing my own files
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 # TestMarkdownToHTML class
 class TestMarkdownToHTML(unittest.TestCase):
@@ -47,6 +47,20 @@ This is the same paragraph on a new line.
                 "- this is a list\n- with items",
             ],
         )
+    
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 # Test Program starts here
