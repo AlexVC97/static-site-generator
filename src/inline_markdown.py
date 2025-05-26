7b93ad2cc3b3fea5369dcb0fb8acef628fb33d0e
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-05-26 08:23:34 +0200

    combined all the splitting functions together

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -72,6 +72,16 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
+# Function: time to put all the splitting functions together
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 # Function: extract any markdown images
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
