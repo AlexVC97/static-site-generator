f1f00df2833e22b7a9fdcb1f76a3607d5d686d10
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-06-11 17:41:22 +0200

    generated a webpage & started a simple web server

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import shutil
 
 # Importing my own files
 from copystatic import copy_files_recursive
+from gencontent import generate_page
 
 # Directory paths
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 # Functions
 def main():
@@ -16,6 +19,12 @@ def main():
     
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+    
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html")
+    )
 
 
 # Program starts here
