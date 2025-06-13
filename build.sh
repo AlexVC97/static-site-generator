dfe07017be68c3c976de6402dec09a9b8309a2f9
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-06-13 15:27:06 +0200

    deployed to the internet using GitHub Pages

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/AlexVC97/static-site-generator/"
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 # Importing my own files
 from copystatic import copy_files_recursive
@@ -7,20 +8,25 @@ from gencontent import generate_pages_recursive
 
 # Directory paths
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 # Functions
 def main():
-    print("Deleting public directory...")
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+        
+    print("Deleting doc directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
     
-    print("Copying static files to public directory...")
+    print("Copying static files to doc directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
     
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 # Program starts here
